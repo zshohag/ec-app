420fcfd0f1d97e2d867fb3b3706d69dce13bdee4
zshohag <zds.mee@gmail.com>
2025-07-12 14:14:16 +0600

    api remove

--- src/app/api/emails/send/route.ts
+++
@@ -1,9 +0,0 @@
-// src/app/api/emails/send/route.ts
-import { sendMail } from '@/lib/email';
-import { NextResponse } from 'next/server';
-
-export async function POST(req: Request) {
-  const { to, subject, html } = await req.json();
-  await sendMail(to, subject, html);
-  return NextResponse.json({ message: 'Email sent' });
-}
