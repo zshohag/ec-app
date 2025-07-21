96f554696a0342181c2bd6dfd70d337c57a80d34
zshohag <zds.mee@gmail.com>
2025-07-21 13:21:58 +0600

    code update url

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,28 +1,4 @@
 
-// import { connectMongoDB } from "@/lib/mongodb";
-// import User from "@/models/User";
-
-// export async function POST(req: Request) {
-//   try {
-//     await connectMongoDB();
-//     const { name, email } = await req.json();
-
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       console.log("🟡 User already exists:", email);
-//       return new Response("User already exists", { status: 200 });
-//     }
-
-//     const newUser = await User.create({ name, email });
-//     console.log("🟢 New user saved:", newUser);
-
-//     return new Response("User created", { status: 201 });
-//   } catch (err) {
-//     console.error("🔴 Error saving user:", err);
-//     return new Response("Error creating user", { status: 500 });
-//   }
-// }
-
 // app/api/user/route.ts
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/User";
